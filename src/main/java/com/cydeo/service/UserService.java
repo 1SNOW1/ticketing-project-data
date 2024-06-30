10d60e48d621a77854824e24b47881a4175ca8b6
enesorhan <enestcp@gmail.com>
2024-06-30 20:48:13 +0300

    update section almost done

--- src/main/java/com/cydeo/service/UserService.java
+++ src/main/java/com/cydeo/service/UserService.java
@@ -10,6 +10,7 @@ public interface UserService {
     UserDTO findByUserName(String username);
     void save(UserDTO user);
     void deleteByUserName(String username);
+    UserDTO update(UserDTO user);
 
 
 }
